f003df385e43f3231b3ab6fa0ed2cf09bd710f0b
root <iseki@rsch.tuis.ac.jp>
2021-09-12 21:11:33 +0900

    log of start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -2,9 +2,11 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 #
-# This is modified by Fumi.Iseki for MDLDockerSpawner 2021 09/10 v0.92
-#                                       Don't stop! Go! Go! Go!!
+# /usr/local/bin/start.sh   2021 09/12 v0.95
+#       This is modified by Fumi.Iseki for MDLDockerSpawner 
 #
+
+PRG_NAME="start.sh"
 
 set -e
 
@@ -20,24 +22,24 @@ run-hooks () {
     if [[ ! -d "$1" ]] ; then
         return
     fi
-    echo "$0: running hooks in $1"
+    echo "$PRG_NAME: running hooks in $1"
     for f in "$1/"*; do
         case "$f" in
             *.sh)
-                echo "$0: running $f"
+                echo "$PRG_NAME: running $f"
                 source "$f"
                 ;;
             *)
                 if [[ -x "$f" ]] ; then
-                    echo "$0: running $f"
+                    echo "$PRG_NAME: running $f"
                     "$f"
                 else
-                    echo "$0: ignoring $f"
+                    echo "$PRG_NAME: ignoring $f"
                 fi
                 ;;
         esac
     done
-    echo "$0: done running hooks in $1"
+    echo "$PRG_NAME: done running hooks in $1"
 }
 
 run-hooks /usr/local/bin/start-notebook.d
@@ -54,20 +56,25 @@ if [ $(id -u) == 0 ] ; then
             mkdir $HOME_DIR
         fi
     fi
+    echo "$PRG_NAME: setup home directory to $HOME_DIR"
 
     #
     # create user account
     if id jovyan &> /dev/null ; then
         usermod -d $HOME_DIR/$NB_USER -l $NB_USER jovyan
     fi
-    #
     groupadd -f -g $NB_GID $NB_GROUP || true
     if [ ! $(id -u $NB_USER 2>/dev/null) ]; then
         useradd --home $HOME_DIR/$NB_USER -u $NB_UID -g $NB_GID -l $NB_USER -s /bin/bash || true
     fi
+    #
+    echo "$PRG_NAME: setup user to $NB_USER"
+    echo "$PRG_NAME: /etc/passwd is ..."
+    cat /etc/passwd | tail -3
 
     #
     # setup home directory
+    echo "$PRG_NAME: start to relocate home dir"
     if [ "$NB_USER" != "jovyan" ]; then
         #
         if [ ! -e "$HOME_DIR/$NB_USER" ]; then
@@ -81,9 +88,10 @@ if [ $(id -u) == 0 ] ; then
             chmod 0700 $HOME_DIR/$NB_USER
             chmod 0700 $HOME_DIR/$NB_USER/* || true 
         fi
-        #
         rm -rf /home/jovyan || true
-    fi
+        # 
+        echo "$PRG_NAME: relocated home dir to $HOME_DIR/$NB_USER"
+    fi 
 
     #
     # setup Jupyter work directory
@@ -98,6 +106,7 @@ if [ $(id -u) == 0 ] ; then
             chown $CHOWN_EXTRA_OPTS $NB_UID:$NB_GID $extra_dir || true
         done
     fi
+    echo "$PRG_NAME: setup Jupyter work directory to $HOME_DIR/$NB_USER/$PRJCT_DIR/$WORK_DIR ($NB_UID:$NB_GID)"
 
     # clean up and path to home directory
     cd $HOME_DIR/$NB_USER/$PRJCT_DIR/$WORK_DIR
@@ -109,9 +118,11 @@ if [ $(id -u) == 0 ] ; then
     if [ ! -e home ]; then 
         ln -s .. home || true
     fi
+    echo "$PRG_NAME: cleaned up and path to home directory"
 
     #
     # setup new teacher group for docker volumes
+    echo "$PRG_NAME: setup new teacher group for docker volumes"
     EGID=$NB_GID
     if [ "$NB_TEACHER" == "$NB_USER" ]; then
         if [ "$NB_THRGID" != "" ]; then
@@ -125,6 +136,10 @@ if [ $(id -u) == 0 ] ; then
         if [ "$EGID" == "" ]; then
             EGID=$NB_GID
         fi 
+        #
+        echo "$PRG_NAME: setup new teacher group ($EGID) for docker volumes"
+        echo "$PRG_NAME: /etc/group is ..."
+        cat /etc/group | tail -3
     fi
 
     #
@@ -132,9 +147,14 @@ if [ $(id -u) == 0 ] ; then
     NB_VOLUMES=`echo $NB_VOLUMES | sed -e "s/[*;$\!\"\'&|\\<>?^%\(\)\{\}\n\r~]//g"`
     NB_SUBMITS=`echo $NB_SUBMITS | sed -e "s/[*;$\!\"\'&|\\<>?^%\(\)\{\}\n\r~]//g"`
     NB_PRSNALS=`echo $NB_PRSNALS | sed -e "s/[*;$\!\"\'&|\\<>?^%\(\)\{\}\n\r~]//g"`
+    #
+    echo "$PRG_NAME: task volumes are     : $NB_VOLUMES"
+    echo "$PRG_NAME: submit volumes are   : $NB_SUBMITS"
+    echo "$PRG_NAME: personal volumes are : $NB_PRDNALS"
 
     cd $HOME_DIR/$NB_USER/$PRJCT_DIR/$WORK_DIR
 
+    echo "$PRG_NAME: start to link to task volumes"
     if [ "$NB_VOLUMES" != "" ]; then
         for VOLUME in $NB_VOLUMES; do
             FL=`echo $VOLUME | cut -d ':' -f 1`
@@ -158,9 +178,11 @@ if [ $(id -u) == 0 ] ; then
                 fi
             fi
         done
+        echo "$PRG_NAME: linked to task volumes"
     fi
 
     #
+    echo "$PRG_NAME: start to link to submit volumes"
     if [ "$NB_SUBMITS" != "" ]; then
         for SUBMIT in $NB_SUBMITS; do
             FL=`echo $SUBMIT | cut -d ':' -f 1`
@@ -184,9 +206,11 @@ if [ $(id -u) == 0 ] ; then
                 fi
             fi
         done
+        echo "$PRG_NAME: linked to submit volumes"
     fi
 
     #
+    echo "$PRG_NAME: start to link to personal volumes"
     if [ "$NB_PRSNALS" != "" ]; then
         for PRSNAL in $NB_PRSNALS; do
             FL=`echo $PRSNAL | cut -d ':' -f 1`
@@ -216,10 +240,12 @@ if [ $(id -u) == 0 ] ; then
                 fi
             fi
         done
+        echo "$PRG_NAME: linked to personal volumes"
     fi
    
     #
-    # fixed user information
+    # fix user information
+    echo "$PRG_NAME: start to fix user information."
     if [[ "$NB_UID" != "$(id -u $NB_USER)" || "$NB_GID" != "$(id -g $NB_USER)" ]]; then
         if [ "$NB_GID" != "$(id -g $NB_USER)" ]; then
             groupadd -f -g $NB_GID -o ${NB_GROUP:-${NB_USER}}
@@ -229,21 +255,35 @@ if [ $(id -u) == 0 ] ; then
         if [ "$NB_TEACHER" == "$NB_USER" ]; then
             usermod -aG $NB_THRGROUP $NB_USER
         fi
+        # 
+        echo "$PRG_NAME: fixed user information."
+        echo "$PRG_NAME: /etc/passwd is ..."
+        cat /etc/passwd | tail -3
     fi
 
     #
     # Enable sudo if requested
+    echo "$PRG_NAME: start to grant sudo access"
     if [[ "$GRANT_SUDO" == "1" || "$GRANT_SUDO" == 'yes' ]]; then
-        echo "$NB_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/notebook  || true
-        chmod 4111 /usr/bin/sudo || true
+        if [ -x /usr/bin/sudo ] ;then
+            echo "$PRG_NAME: granting $NB_USER sudo access and appending $CONDA_DIR/bin to sudo path"
+            if [ ! -d /etc/sudoers.d ]; then
+                mkdir /etc/sudoers.d
+            fi
+            echo "$NB_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/notebook 
+            chmod 4111 /usr/bin/sudo 
+            # Add $CONDA_DIR/bin to sudo secure_path
+            sed -r "s#Defaults\s+secure_path\s*=\s*\"?([^\"]+)\"?#Defaults secure_path=\"\1:$CONDA_DIR/bin\"#" /etc/sudoers | \
+                        grep secure_path > /etc/sudoers.d/path 
+        fi
     fi
-    # Add $CONDA_DIR/bin to sudo secure_path
-    sed -r "s#Defaults\s+secure_path\s*=\s*\"?([^\"]+)\"?#Defaults secure_path=\"\1:$CONDA_DIR/bin\"#" /etc/sudoers | grep secure_path > /etc/sudoers.d/path || true
 
     # Exec the command as NB_USER with the PATH and the rest of
     # the environment preserved
     run-hooks /usr/local/bin/before-notebook.d
+    echo "$PRG_NAME: executing the command: ${cmd[@]}"
     exec sudo -E -H -u $NB_USER PATH=$PATH XDG_CACHE_HOME=$HOME_DIR/$NB_USER/.cache PYTHONPATH=${PYTHONPATH:-} "${cmd[@]}" 
+    #
 else
     if [[ "$NB_UID" == "$(id -u jovyan 2>/dev/null)" && "$NB_GID" == "$(id -g jovyan 2>/dev/null)" ]]; then
         # User is not attempting to override user/group via environment
