db699f1792d2734bdcc798bb7e85da49d3d1a740
iseki <iseki@rsch.tuis.ac.jp>
2022-01-14 16:18:13 +0900

    dockerpush.sh

--- sh/dockerpush.sh
+++ sh/dockerpush.sh
@@ -3,8 +3,9 @@
 DKRREP="www.nsl.tuis.ac.jp:5000"
 
 #
-LST=`docker images | grep ltctr | awk -F" " '{print $1":"$2}' | grep -v "$DKRREP" | sed -e "s/localhost\///" `
+LST=`docker images | grep ltictr | awk -F" " '{print $1":"$2}' | grep -v "$DKRREP" | sed -e "s/localhost\///" `
 
+echo $LST
 for IMG in $LST ; do
     REP=`echo $IMG | awk -F":" '{print $1}'`
     REP=$DKRREP"/"$REP
