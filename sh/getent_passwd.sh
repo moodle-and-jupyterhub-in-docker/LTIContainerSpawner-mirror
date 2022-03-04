2ba2a8e2f34c1e410a836e53cfc119813c876162
root <iseki@rsch.tuis.ac.jp>
2022-03-04 11:26:32 +0900

    getent

--- sh/getent_passwd.sh
+++ sh/getent_passwd.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-SSH_USER=iseki
-SSH_PASS=*******
 SSH_HOST=202.26.144.41
-CMD="getent passwd"
 
+#
 if [ -n "$PASSWORD" ]; then
   echo "$PASSWORD"
   exit 0
 fi
 
+read -p  "userid: " SSH_USER
+read -sp "passwd: " SSH_PASS
+
+#SSH_USER=
+#SSH_PASS=
+
+if [ "$SSH_USER" = "" -o "$SSH_PASS" = "" ]; then
+    exit 1
+fi
+
+CMD="getent passwd"
+
 export PASSWORD=$SSH_PASS
 export SSH_ASKPASS=$0
 export DISPLAY=:0.0
 
-setsid ssh $SSH_USER@$SSH_HOST $CMD 
-#exec setsid ssh $SSH_USER@$SSH_HOST $CMD | get_valid_user.sh
+#setsid ssh $SSH_USER@$SSH_HOST $CMD 
+exec setsid ssh $SSH_USER@$SSH_HOST $CMD | get_valid_user.sh
