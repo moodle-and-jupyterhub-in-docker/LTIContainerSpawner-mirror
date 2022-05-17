7bef0957828b5eda0316be934a22937097edc80a
iseki <iseki@rsch.tuis.ac.jp>
2022-05-17 10:41:42 +0900

    pull

--- sh/dockerpull.sh
+++ sh/dockerpull.sh
@@ -7,7 +7,16 @@ DKRREP="www.nsl.tuis.ac.jp:5000"
 LST="jupyter-base jupyter-singleuser jupyter-datascience jupyter-tensorflow jupyter-scipy vhtec_jupyter xeus-cling java php swift-tensorflow"
 
 for IMG in $LST ; do
-    echo $IDSTR/$IMG
+    echo 
+    echo Pull $IDSTR/$IMG
+    IMGID=`docker images | grep "$DKRREP/$IDSTR/$IMG" | grep latest | awk -F" " '{print $3}'`
     docker pull $DKRREP/$IDSTR/$IMG 
+    #
+    NONE=`docker images | grep $IMGID | grep "\<none\>"`
+    if [ "$NONE" != "" ]; then
+        TMPN="$DKRREP/$IDSTR/$IMG:del"
+        docker tag $IMGID $TMPN
+        docker rmi $TMPN
+    fi   
 done
 
