05e1a4fb94b333bee51661a371d8b35c6251e787
iseki <iseki@rsch.tuis.ac.jp>
2022-06-20 20:36:29 +0900

    fix websocket

--- ltictr_proxy/ltictr_ws.h
+++ ltictr_proxy/ltictr_ws.h
@@ -1,4 +1,5 @@
 
+
 #include "buffer.h"
 #include "tjson.h"
 
@@ -18,9 +19,9 @@ struct  ws_info {
 };
 
 
-tJson* ws_json(Buffer* buf, char* mesg, int cc);
-tJson* ws_json_client(char* mesg, int cc);
-tJson* ws_json_server(char* mesg, int cc);
+//tJson* ws_json(Buffer* buf, char* mesg, int cc);
+tJson* ws_json_parse(ringBuffer* ring);
+tJson* ws_json_client(unsigned char* mesg, int cc);
+tJson* ws_json_server(unsigned char* mesg, int cc);
 
-void  print_buf(FILE* fp, unsigned char* buf, int size);
-
+//void  print_buf(FILE* fp, unsigned char* buf, int size);
